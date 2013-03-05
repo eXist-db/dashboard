c25d58b73156b827ebf86b7f50b32d2efa272183
Leif-Jöran Olsson <ljo@fripost.org>
2013-03-05 23:13:32 +0100

    Adding saucelabs selenium webdriver with initial Firefox5 and IE8 tests.

---
+++ dashboard-webdriver/src/test/java/org/exist/WebDriverIE8Test.java
@@ -0,0 +1,83 @@
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * {@link RemoteWebDriver} test running Selenium tests with <a href="http://saucelabs.com/ondemand">Sauce OnDemand</a>.
+ *
+ * This test also includes the <a href="">Sauce JUnit</a> helper classes, which will use the Sauce REST API to mark the Sauce Job as passed/failed.
+ *
+ * In order to use the {@link SauceOnDemandTestWatcher}, the test must implement the {@link SauceOnDemandSessionIdProvider} interface.
+ *
+ * @author Ross Rowe
+ * @author ljo
+
+ */
+public class WebDriverIE8Test implements SauceOnDemandSessionIdProvider {
+
+    /**
+     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
+     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
+     */
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication();
+
+    /**
+     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
+     */
+    public @Rule
+    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    /**
+     * JUnit Rule which will record the test name of the current test.  This is referenced when creating the {@link DesiredCapabilities},
+     * so that the Sauce Job is created with the test name.
+     */
+    public @Rule TestName testName= new TestName();
+
+    private WebDriver driver;
+
+    private String sessionId;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabillities = DesiredCapabilities.internetExplorer();
+        capabillities.setCapability("version", "8");
+        capabillities.setCapability("platform", Platform.XP);
+        capabillities.setCapability("name",  testName.getMethodName());
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabillities);
+        this.sessionId = ((RemoteWebDriver)driver).getSessionId().toString();
+    }
+
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    @Test
+    public void dashboardTitleIE8() throws Exception {
+        driver.get("http://demo.exist-db.org/exist/apps/dashboard/");
+        assertEquals("Dashboard", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}

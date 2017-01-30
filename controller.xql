eXist-db/dashboard

commit af4cc34b8622a75b5c35289f32e4fab62ccb0207

[bugfix] Make sure not to assume the URL is /exist/ when reverse proxying Related: https://github.com/eXist-db/exist/issues/1122

controller.xql
@@ -26,7 +26,11 @@ if ($exist:path eq '') then
 else if ($exist:path = "/") then
     (: forward root path to index.xql :)
     <dispatch xmlns="http://exist.sourceforge.net/NS/exist">
-        <redirect url="index.html"/>
+        if(request:get-uri() eq "/exist/apps/dashboard/" and request:get-header("X-Forwarded-URI") eq "/apps/dashboard/") then
+            <redirect url="/apps/dashboard/index.html"/>
+        else
+            <redirect url="index.html"/>
+    }
     </dispatch>
 
 else if ($exist:resource = "get-icon.xql") then

commit a4b962b1bbe7235eabd991a680d258cecae496b8

Fix broken login
The bug was introduced in https://github.com/eXist-db/dashboard/commit/af4cc34b8622a75b5c35289f32e4fab62ccb0207 - a missing curly brace.

This same bug is also fixed in #46, but since that PR is not yet pulled in — there are additional changes in that PR that appear to require deeper review — can we just fix this one now?  It's a no-brainer.

controller.xql
@@ -26,6 +26,7 @@ if ($exist:path eq '') then
 else if ($exist:path = "/") then
     (: forward root path to index.xql :)
     <dispatch xmlns="http://exist.sourceforge.net/NS/exist">
+    {
         if(request:get-uri() eq "/exist/apps/dashboard/" and request:get-header("X-Forwarded-URI") eq "/apps/dashboard/") then
             <redirect url="/apps/dashboard/index.html"/>
         else